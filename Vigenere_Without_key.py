1f1656da93a2c4aa0d91197aa17dbdaba46013ae
Siva Girish <sivagirish81@gmail.com>
2019-03-15 22:24:00 +0530

    .

--- Vigenere_Without_key.py
+++ Vigenere_Without_key.py
@@ -1,5 +1,9 @@
 from collections import Counter
 
+#Generates all 26 caesr cipher combinations alphabetically.
+#might contain bugs
+#please check
+
 def gen_26_combn(enc_string):
     temp=enc_string.upper()
     k=0
@@ -14,6 +18,8 @@ def gen_26_combn(enc_string):
         combn.append(string)
     return combn
 
+#Decryptor function once you guess the right key 
+
 def Decryptor(Encrypted_string,key):
     l=len(key)
     j=0
@@ -27,6 +33,8 @@ def Decryptor(Encrypted_string,key):
         dec_string+=chr(dchar)
         j=(j+1)%l
     return dec_string
+
+#Chi square analysis function
 
 def chi_sq(enc_s):
     #exp_count = [0.08167,0.01492,0.02782,0.04253,0.12702,0.02228,0.02015,0.06094,0.06966,0.00153,0.00772,0.04025,0.02406,0.06749,0.07507,0.01929,0.00095,0.05987,0.06327,0.09056,0.02758,0.00978,0.02360,0.00150,0.01974,0.00074]
@@ -43,6 +51,15 @@ def chi_sq(enc_s):
     for i in enc_s:
         chi_sq.append(((p[i]-(expectedvalues[i.lower()]*tcount))**2)/(expectedvalues[i.lower()]*tcount)**2)
     return sum(chi_sq)
+
+#From here's where I went wrong
+#this function basically takes the appropriate key lengths
+#breaks the string in the similar manner
+#generates all 26 possible caese cipher combinations
+#Finds a single alphabet of the key
+
+#This one function has not been implemented properly.
+#If you can help me fix this function then it will work.
 
 def Chk_key(Enc_string,i):
     l=len(Enc_String)
@@ -64,7 +81,11 @@ def Chk_key(Enc_string,i):
         
             
 #Finding Index of Coincdence
-
+#finding index of coincidence for strings of all possible keys
+#breaking the strings into multiple strings of key size
+#Finding index of coincidence of each one of them
+#this will give us a list of appropriate/possible key lengths.
+        
 def IC(string1,k):
     l=len(string1)
     #print(l)
@@ -90,7 +111,7 @@ def IC(string1,k):
         
         
 
-
+#Function to calculate index of coincedence of a string
 def get_ic(s):
     n=len(s)
     ic=0
@@ -98,7 +119,12 @@ def get_ic(s):
         ic=(1/(float(n)*(n-1)))*(sum([s.count(a)*(s.count(a)-1) for a in set(s)]))
     return ic
 
-                
+#Removing all spaces from the string and passing the string to index of coincience function for different keys.
+#The Index of Coincidence (I.C.) is a statistical technique that gives an indication of how English-like a piece of text is.
+#One of the useful properties of the technique is that the result of the I.C. does not change if you apply a substitution cipher to the text.
+#This is because the I.C. is based on letter frequencies, and simple substitution ciphers do not modify the individual letter frequencies.
+#If text is similar to english it will have an I.C. of around 0.06, if the characters are uniformly distributed the I.C. is closer to 0.03-0.04.
+#what we are basically trying to do is find the period of repitition which will give us the key length.
 def IOC(Enc_string):
     i=1
     mod_string="".join(Enc_string.split())
