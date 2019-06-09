a474ea603335cd529741a92c8eb1df2251b37e90
Siva Girish <sivagirish81@gmail.com>
2019-06-09 12:21:22 +0530

    Rail-Fence Complete

--- Ciphers/Day 4/Rail-Fence/Rail-Fence.py
+++ Ciphers/Day 4/Rail-Fence/Rail-Fence.py
@@ -5,33 +5,72 @@ The rail fence cipher works by writing the message on alternate lines across the
 """
 
 def Encryptor(word,key):
-    res=[[] for i in range(key)]
-    length=[]
-    eword=""
-    kf=(2*key-1)
+    down=0
+    Rf=[[-1 for i in range(len(word))]for j in range(key)]
+    print(Rf)
+    c=0
+    r=0
     for i in range(len(word)):
-        res[i%key].append(i)
-    print(res)
-    for i in range(key):
-        length.append(len(res[i]))
-    s=min(length)
-    for i in range(key):
-        for j in range(s):
-            eword+=word[res[i][j]]
-    for i in range(key):
-        if (len(res[i])==(s+1)):
-            eword+=word[res[i][s]]
-    return eword
-        
-                      
+        if (r==0) or (r==(key-1)):
+            down=not down
+        Rf[r][c]=word[i]
+        c+=1
+        if (down==1):
+            r+=1
+        else:
+            r-=1
+    print(Rf)
+    elist = [] 
+    for i in range(key): 
+        for j in range(len(word)): 
+            if Rf[i][j] != -1: 
+                elist.append(Rf[i][j])
+    return "".join(elist)
+
+def get_Rail_Matrix(word,key):
+    down=0
+    Rf=[['_' for i in range(len(word))]for j in range(key)]
+    #print(Rf)
+    c=0
+    r=0
+    for i in range(len(word)):
+        if (r==0) or (r==(key-1)):
+            down=not down
+        Rf[r][c]='$'
+        c+=1
+        if (down==1):
+            r+=1
+        else:
+            r-=1
+    return Rf
     
-        
-            
+def Decryptor(ciphertext,key):
+    RM=get_Rail_Matrix(ciphertext,key)
+    k=0
+    for i in range(key):
+        for j in range(len(ciphertext)):
+            if (RM[i][j]=='$') and (k<len(ciphertext)):
+                RM[i][j]=ciphertext[k]
+                k+=1
+    r,c=0,0
+    down=0
+    dlist=[]
+    for i in range(len(ciphertext)): 
+        if (r==0) or (r==(key-1)):
+            down=not down
+        if (RM[r][c] != '$'): 
+            dlist.append(RM[r][c]) 
+            c += 1
+        if down: 
+            r += 1
+        else: 
+            r-= 1
+    return("".join(dlist)) 
+    
+    
 
-
-
-Word=input("Enter the word to be Encrypted")
-key=int(input("Eter the key to be used"))
+Word=input("Enter the word to be Encrypted : ")
+key=int(input("Eter the key to be used : "))
 Res=Encryptor(Word,key)
 print("The Encrypted string is : ",Res)
-#print("The Decrypted string is : ",Decryptor(Res,key))
+print("The Decrypted string is : ",Decryptor(Res,key))
