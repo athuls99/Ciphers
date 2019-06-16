fe1cbddf359cfa2618cf649b4a51170e77f68237
Gauravcg492 <officialgauravcg492@gmail.com>
2019-06-16 17:09:36 +0530

    Hill done.

---
+++ Ciphers/Day 2/Hill/HillGUI.py
@@ -0,0 +1,84 @@
+from functions import textEncDec,isPerfect,createMatrix,checkKey
+import tkinter as tk
+from tkinter import ttk
+
+class Start:
+    def __init__(self,master):
+        self.frame = tk.Frame(master,width=700,height=200)
+        self.frame.pack(pady=1,padx=10)
+        labelA = tk.Label(self.frame,text="Key",fg="black",font="Arial 15")
+        self.a = tk.Text(self.frame,height=1,width=20,font="Times 15")
+        labelA.pack(side=tk.LEFT,padx=5)
+        self.a.pack(padx=1)
+
+class Cipher:
+    def __init__(self,master,obj):
+        self.key = obj.a
+        self.msg = ""
+
+        # for encrypt
+        self.frame = tk.Frame(master,width=650,height=200,bd=1,relief=tk.RAISED)
+        self.frame.pack(pady=25,padx=10,fill=tk.X)
+        self.text = tk.Text(self.frame,height=5,width=50,fg="black",font="Times 15")
+        self.text.pack(side=tk.LEFT,padx=10,pady=15)
+        self.ebutton = tk.Button(self.frame,text="Encrypt",command=self.enc,font="Arial 20")
+        self.ebutton.pack(padx=10,pady=70)
+
+        # for decrypt
+        self.frame = tk.Frame(master,width=650,height=200,bd=1,relief=tk.RAISED)
+        self.frame.pack(pady=5,padx=10,fill=tk.X)
+        self.etext = tk.Text(self.frame,height=5,width=50,fg="black",font="Times 15")
+        self.etext.pack(side=tk.LEFT,padx=10,pady=15)
+        self.dbutton = tk.Button(self.frame,text="Decrypt",command=self.dec,font="Arial 20")
+        self.dbutton.pack(padx=10,pady=70)
+    
+    def enc(self):
+        chk = self.check()
+        if chk:
+            text = get(self.text)
+            etext = textEncDec(text.upper(),chk,0)
+            self.etext.delete("1.0","end")
+            self.etext.insert(tk.END,etext)
+    
+    def dec(self):
+        chk = self.check()
+        if chk:
+            etext = get(self.etext)
+            text = textEncDec(etext.upper(),chk,1)
+            self.text.delete("1.0","end")
+            self.text.insert(tk.END,text)
+
+    def popup(self):
+        popup = tk.Tk()
+        popup.wm_title("!")
+        label = ttk.Label(popup, text=self.msg, font="Verdana 10")
+        label.pack(side="top", fill="x", pady=10,padx=10)
+        B1 = ttk.Button(popup, text="Okay", command = popup.destroy)
+        B1.pack()
+        popup.mainloop()
+
+    def check(self):
+        key = get(self.key).upper()
+        lKey = len(key)
+        if isPerfect(lKey):
+            order = int(lKey**(1/2))
+            keyMat = createMatrix(key,order,0)
+            det = keyMat.det()
+            if checkKey(det):
+                return key
+            else:
+                self.msg = "The Key is not invertible"
+        else:
+            self.msg = "The length of key should be Perfect Sqaure"
+        self.popup()
+        return False
+
+def get(obj):
+    return obj.get("1.0","end").strip()
+
+if __name__ == "__main__":
+    window = tk.Tk()
+    #window.geometry("700x700")
+    sobj = Start(window)
+    enc = Cipher(window,sobj)
+    window.mainloop()
--- Ciphers/Day 2/Hill/functions.py
+++ Ciphers/Day 2/Hill/functions.py
@@ -1,6 +1,7 @@
 from sympy import Matrix
 import math
 import re
+import inspect
 
 def isPerfect(n):
     root = math.sqrt(n)
@@ -10,12 +11,12 @@ def isPerfect(n):
 
 def createMatrix(text,order,opt):
     if opt == 0:
-        Mat = Matrix(order,order,[ord(char)-97 for char in text])
+        Mat = Matrix(order,order,[ord(char)-65 for char in text])
     elif opt == 1:
         textL = len(text)
         rows = math.ceil(textL/order)
         totalLen = rows*order
-        Mat = Matrix(rows,order,[ord(char)-97 for char in text]+['x' for x in range(totalLen - textL)])
+        Mat = Matrix(rows,order,[ord(char)-65 for char in text]+[ord('X')-65 for x in range(totalLen - textL)])
     return Mat  
 
 def checkKey(n):
@@ -23,32 +24,56 @@ def checkKey(n):
         return True
     return False
 
+def getMod(det):
+    for i in range(1,26):
+        if (i*det)%26 == 1:
+            return i
+
+
+def getInverse(key,det):
+    det = int(det%26)
+    dInv = getMod(det)
+    nkey = dInv*(key.adjugate())
+    return nkey
+
 def crypt(tMat,kMat,order):
     rows = tMat.rows
     encMat = Matrix()
     for i in range(rows):
         encRow = kMat*(tMat[i,:].transpose())
         encMat = Matrix([encMat,[char%26 for char in encRow]])
-    text = "".join([chr(val+97) for val in encMat])
+    text = "".join([chr(val+65) for val in encMat])
     return text
 
-def encryptText(text,key):
+def textEncDec(text,key,opt):
     lKey = len(key)
     if isPerfect(lKey):
         order = int(lKey**(1/2))
         keyMat = createMatrix(key,order,0)
         det = keyMat.det()
         if checkKey(det):
+            text = re.sub(r'[^A-Za-z]',"",text)
             textMat = createMatrix(text,order,1)
-            encText = crypt(textMat,keyMat,order)
-            print("The encrypted text is: ",encText)
+            if opt == 0:
+                encText = crypt(textMat,keyMat,order)
+                return encText
+            elif opt == 1:
+                newKey = getInverse(keyMat,det)
+                decText = crypt(textMat,newKey,order)
+                return decText.lower()
         else:
             print("The key is not Invertible!!")
     else:
         print("The length of key should be Perfect Square!!")
 
 def encryptor():
-    inpText = input("Enter the Text: ")
-    inpText = re.sub(r'[^A-Za-z]',"",inpText)
-    key = input("Enter the key: ")
-    encryptText(inpText.lower(),key.lower())
+    inpText = input("Enter the Text: ").upper()
+    key = input("Enter the key: ").upper()
+    etext = textEncDec(inpText.upper(),key.upper(),0)
+    print("The encrypted text is: ",etext)
+
+def decryptor():
+    text = input("Enter the Encrypted Text: ").upper()
+    key = input("Enter the key: ").upper()
+    dtext = textEncDec(text,key,1)
+    print("The decrypted text is: ",dtext)
